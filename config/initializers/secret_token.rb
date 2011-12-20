277a617cab85889e0aa2ce6e204c7216bed4e39f
Jarek <banyaai[at]gmail.com>
2011-12-20 20:31:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FhtelektroRor::Application.config.secret_token = '5421ca1e7c276f36708c80086346cb8b6b95dfcb15a2881c43de8f48533d574ea7288d5dc764d5568ca885cba1734b7c240e5cfeadf0863af8fed6ddfb01c73c'
